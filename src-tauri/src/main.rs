e034fb3e1b74a5f05bc07d3caa985adac04efdcb
drcheeseface <tharun1@hotmail.co.uk>
2024-09-08 17:28:26 +0100

    chore: SOME kind of progress IS BEING MADE!!!

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,81 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use mahc::hand::error::HandErr;
+
 #[tauri::command]
 fn adder(num1: u16, num2: u16) -> String {
-    println!("HIHIHIHIHIH");
     let num3 = num1 + num2;
     format!("this is you number {}", num3)
 }
 
+#[tauri::command]
+fn calc(input: String) -> Vec<String> {
+    use mahc::hand::Hand;
+    use mahc::tile_group::TileGroup;
+
+    let tiles: Result<Vec<TileGroup>, HandErr> = input
+        .split_whitespace()
+        .map(|x| x.to_string().try_into().map_err(|e| e))
+        .collect();
+    match tiles {
+        Err(e) => return vec![format!("Error: {}", e)],
+        _ => (),
+    }
+
+    let tiles = tiles.unwrap();
+    let win_tile: TileGroup = "Ew".to_string().try_into().unwrap();
+    let prev_tile: TileGroup = "Ew".to_string().try_into().unwrap();
+    let seat_tile: TileGroup = "Ew".to_string().try_into().unwrap();
+
+    let hand: Result<Hand, mahc::hand::error::HandErr> =
+        mahc::hand::Hand::new(tiles, win_tile, seat_tile, prev_tile);
+    let honba = mahc::score::HonbaCounter::default();
+    if hand.is_err() {
+        let err = hand.err().unwrap();
+        return vec![format!("Error: {}", err)];
+    }
+
+    let score = mahc::calc::get_hand_score(
+        hand.unwrap(),
+        None,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        false,
+        honba,
+    );
+    if score.is_err() {
+        let err = score.err().unwrap();
+        return vec![format!("Error: {}", err)];
+    }
+    let score = score.unwrap();
+
+    let payment = score.payment();
+    let dealer_ron = payment.dealer_ron(score.honba());
+    let dealer_tsumo = payment.dealer_tsumo(score.honba());
+    let non_dealer_ron = payment.non_dealer_ron(score.honba());
+    let non_dealer_tsumo_to_dealer = payment.non_dealer_tsumo_to_dealer(score.honba());
+    let non_deadler_tsumo_to_non_dealer = payment.non_dealer_tsumo_to_non_dealer(score.honba());
+    vec![
+        format!("Dealer Ron: {}", dealer_ron),
+        format!("Dealer Tsumo: {}", dealer_tsumo),
+        format!("Non-Dealer Ron: {}", non_dealer_ron),
+        format!("Non-Dealer Tsumo to Dealer: {}", non_dealer_tsumo_to_dealer),
+        format!(
+            "Non-Dealer Tsumo to Non-Dealer: {}",
+            non_deadler_tsumo_to_non_dealer
+        ),
+    ]
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![adder])
+        .invoke_handler(tauri::generate_handler![adder, calc])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,15 @@ import { invoke } from "@tauri-apps/api/tauri";
 
 let num1: HTMLInputElement | null;
 let num2: HTMLInputElement | null;
+let input: HTMLInputElement | null;
+let btn: HTMLInputElement | null;
 let added: HTMLElement | null;
+let output: HTMLElement | null;
 
 async function adder() {
     if (num1 && num2 && added) {
         var asnum1: number = +num1.value;
         var asnum2: number = +num2.value;
-        // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
         added.textContent = await invoke("adder", {
             num1: asnum1,
             num2: asnum2
@@ -16,13 +18,37 @@ async function adder() {
     }
 }
 
+async function calc() {
+    console.log("calc");
+    if (input && output) {
+        console.log("input: ", input.value);
+        let list: string[] = await invoke("calc", {
+            input: input.value
+        });
+        if (list.length == 1) {
+            output.textContent = list[0];
+        }
+        output.innerHTML = list.join("<br>");
+    }
+}
+
+
 window.addEventListener("DOMContentLoaded", () => {
     num1 = document.querySelector("#num1");
     num2 = document.querySelector("#num2");
     added = document.querySelector("#added");
+    input = document.querySelector("#input");
+    btn = document.querySelector("#input-btn");
+    output = document.querySelector("#output");
+
     document.querySelector("#adder-form")?.addEventListener("submit", (e) => {
         e.preventDefault();
         adder();
     });
+    btn?.addEventListener("click", (e) => {
+        e.preventDefault();
+        calc();
+    });
+
 });
 
